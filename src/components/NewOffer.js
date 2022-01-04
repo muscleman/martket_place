fe98da71c983e09adc4a2c840bd8a77cb7074b2c
PRavaga <trueravaga@gmail.com>
2022-01-05 00:20:25 +0300

    added offer submission popup

--- src/components/NewOffer.js
+++ src/components/NewOffer.js
@@ -1,18 +1,66 @@
 import React from "react";
+import tshirt from "./../img/Tshirt-design.jpg";
 
 const NewOffer = () => {
   return (
-    <div className="offerPopup">
-      Offer popup
-      <form className="offerForm">
-        <input type="text" className="offerTitle" />
-        <input type="text" className="offerDescription" />
-        <input type="text" className="offerCategory" />
-        <input type="text" className="offerPrice" />
-        <input type="text" className="offerImg" />
-        <input type="text" className="offerContacts" />
-        <input type="text" className="offerComments" />
+    <div className="popup-container">
+      <form className="offer-form">
+        <div>
+          <label for="offer-title">Title</label>
+          <input type="text" id="offer-title" required />
+        </div>
+        <div>
+          <label for="offer-description">Description</label>
+          <input type="text" id="offer-description" />
+        </div>
+        <div>
+          <label for="offer-category">Category</label>
+          <input type="text" id="offer-category" />
+        </div>
+        <div>
+          <label for="offer-price">Price</label>
+          <input type="text" id="offer-price" />
+        </div>
+        <div>
+          <label for="offer-img">Image link</label>
+          <input type="text" id="offer-img" />
+        </div>
+        <div>
+          <label for="offer-contact">Contact details</label>
+          <input type="text" id="offer-contact" />
+        </div>
+        <div>
+          <label for="offer-comment">Comments</label>
+          <input type="text" id="offer-comment" />
+        </div>
       </form>
+      <div className="offer-preview">
+        <p className="offer-preview-text">Preview:</p>
+        <div className="offer-card">
+          <img className="offer-img" src={tshirt} alt="Marketplace offer pic" />
+          <div className="offer-content">
+            <h3 className="offer-title">Title</h3>
+            <p className="offer-categories">Category</p>
+            <p className="offer-price">
+              <strong>100</strong> $ZANO
+            </p>
+            <p className="offer-description">Comments</p>
+
+            <p className="offer-contact">Contacts</p>
+          </div>
+
+          <div className="offer-buttons">
+            <a href="#" className="btn-primary">
+              Respond
+            </a>
+          </div>
+        </div>
+      </div>
+      <div>
+        <a href="#" className="btn-popup">
+          Submit offer
+        </a>
+      </div>
     </div>
   );
 };
